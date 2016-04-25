ef32001f07bfeb25c7dfbb9dec7d154d1750704f
jorissonck <joris_sonck@telenet.be>
2016-04-25 23:15:33 +0200

    Hamburger menu
    Menu gaat open en dicht!

---
+++ game/PinAnt/Assets/Scripts/CloseMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloseMenu : MonoBehaviour {
+
+    public GameObject menu;
+
+    public void Close_Menu()
+    {
+        menu.SetActive(false);
+    }
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ game/PinAnt/Assets/Scripts/OpenMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenMenu : MonoBehaviour {
+
+    public GameObject menu;
+
+     public void Toggle_Menu()
+    {
+
+
+        Debug.Log("click");
+            menu.SetActive(true);
+        
+    }
+
+
+}
