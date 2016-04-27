33f2acad07f40c8a0c22cc04969cc114cedda2d0
MazurekPiotr <mazurek.piotr@student.kdg.be>
2016-04-27 15:57:01 +0100

    seed project table en users-projects table

--- web/projectantwerpen/database/seeds/DatabaseSeeder.php
+++ web/projectantwerpen/database/seeds/DatabaseSeeder.php
@@ -21,5 +21,36 @@ class DatabaseSeeder extends Seeder
              array('name'=>'Ronny','email'=>'ronny@test.be', 'password' => Hash::make( 'password' ), 'type_of_user'=>'regular')
 
           ));
+
+
+         DB::table('projects')->delete();
+         //insert some dummy records
+         DB::table('projects')->insert(array(
+             array('titel' => 'Museum aan de stroom', 'categorie' => 'cultuur', 'uitleg' => '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+             	sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+             	Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+             	Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+             	'fotonaam' => 'mas.jpg', 'einddatum' => 'mei 2011', 'kostprijs' => '55 miljoen euro', 'fase' => '4', 'likes' => '12345', 'aantal_bekeken' => '65432'),
+             array('titel' => 'Museum aan de stroom', 'categorie' => 'cultuur', 'uitleg' => '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+             	sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+             	Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+             	Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+             	'fotonaam' => 'mas.jpg', 'einddatum' => 'mei 2011', 'kostprijs' => '55 miljoen euro', 'fase' => '4', 'likes' => '12345', 'aantal_bekeken' => '65432'),
+             array('titel' => 'Museum aan de stroom', 'categorie' => 'cultuur', 'uitleg' => '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+             	sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+             	Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+             	Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+             	'fotonaam' => 'mas.jpg', 'einddatum' => 'mei 2011', 'kostprijs' => '55 miljoen euro', 'fase' => '4', 'likes' => '12345', 'aantal_bekeken' => '65432')
+          ));
+
+		DB::table('users_projects')->delete();
+         //insert some dummy records
+         DB::table('users_projects')->insert(array(
+             array('fk_user' => '3', 'fk_project' => '1'),
+             array('fk_user' => '5', 'fk_project' => '2'),
+             array('fk_user' => '4', 'fk_project' => '3'),
+             array('fk_user' => '4', 'fk_project' => '2'),
+             array('fk_user' => '3', 'fk_project' => '1')
+          ));
     }
 }
