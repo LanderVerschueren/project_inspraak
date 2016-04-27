1fc9aeff5b52eb4ae35b70e4e7a5899fd60e5e70
Bert Van Hove <bert.vanhove@student.kdg.be>
2016-04-27 02:49:25 +0200

    Toggle_Buttons added
    Bug: werkt niet als je het 2x na elkaar doet

---
+++ game/PinAnt/Assets/Scripts/ToggleButtons.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ToggleButtons : MonoBehaviour 
+{
+  public Button likeButton;
+  public Button dislikeButton;
+  public Button commentButton;
+  public bool isEnabled;
+	// Use this for initialization
+	void Start () 
+  {
+    
+	}
+
+  public void Toggle_Buttons()
+  {
+    isEnabled =! isEnabled;
+    likeButton.enabled = isEnabled;
+    dislikeButton.enabled = isEnabled;
+    commentButton.enabled = isEnabled;
+  }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
