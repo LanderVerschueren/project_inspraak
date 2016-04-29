bfb0f3d4a7bab45b2bea195db1a835759c287170
MazurekPiotr <mazurek.piotr@student.kdg.be>
2016-04-29 10:45:38 +0100

    seed verandert

--- web/projectantwerpen/database/seeds/DatabaseSeeder.php
+++ web/projectantwerpen/database/seeds/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
          //insert some dummy records
          DB::table('users')->insert(array(
              array('name'=>'Piotr Mazurek','email'=>'mazurek.piotr@student.kdg.be', 'password' => Hash::make( 'projectant' ), 'type_of_user'=>'admin'),
-             array('name'=>'Lander Verschueren','email'=>'lander.erschueren@student.kdg.be', 'password' => Hash::make( 'projectant' ), 'type_of_user'=>'admin'),
+             array('name'=>'Lander Verschueren','email'=>'lander.verschueren@student.kdg.be', 'password' => Hash::make( 'projectant' ), 'type_of_user'=>'admin'),
              array('name'=>'John','email'=>'john@test.be', 'password' => Hash::make( 'password' ), 'type_of_user'=>'regular'),
              array('name'=>'Alexander','email'=>'alexander@test.be', 'password' => Hash::make( 'password' ), 'type_of_user'=>'regular'),
              array('name'=>'Ronny','email'=>'ronny@test.be', 'password' => Hash::make( 'password' ), 'type_of_user'=>'regular')
