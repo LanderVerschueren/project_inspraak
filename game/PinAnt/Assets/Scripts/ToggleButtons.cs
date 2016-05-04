880fe3cfd1cf0c0bf1e2b752a92470f3d5d451c6
jorissonck <joris_sonck@telenet.be>
2016-05-04 14:34:12 +0200

    unknown

--- game/PinAnt/Assets/Scripts/ToggleButtons.cs
+++ game/PinAnt/Assets/Scripts/ToggleButtons.cs
@@ -8,22 +8,33 @@ public class ToggleButtons : MonoBehaviour
   public Button dislikeButton;
   public Button commentButton;
   public bool isEnabled;
+
+    private int likes = 0;
+    private int dislikes = 0;
+
+
+
+
+
+
 	// Use this for initialization
 	void Start () 
   {
     
 	}
 
-  public void Toggle_Buttons()
-  {
-    isEnabled =! isEnabled;
-    likeButton.enabled = isEnabled;
-    dislikeButton.enabled = isEnabled;
-    commentButton.enabled = isEnabled;
-  }
 
-	// Update is called once per frame
-	void Update () {
+
+    public void Toggle_Buttons()
+    {
+        isEnabled = !isEnabled;
+        likeButton.enabled = isEnabled;
+        dislikeButton.enabled = isEnabled;
+        commentButton.enabled = isEnabled;
+    }
+
+    // Update is called once per frame
+    void Update () {
 	
 	}
 }
