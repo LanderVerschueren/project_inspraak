b2fa4900b2cc1ea9030e2c52712bee2b83d32fab
jorissonck <joris_sonck@telenet.be>
2016-05-04 15:28:55 +0200

    Buttons macheren deftig

---
+++ game/PinAnt/Assets/Scripts/ToggleLikeDislike.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ToggleLikeDislike : MonoBehaviour {
+
+
+
+    public Text likeButtonText;
+    public Text dislikeButtonText;
+
+
+    private bool isLiked = false;
+    private bool isDisliked = false;
+
+
+    public void ClickLike()
+    {
+        if(isLiked == false && isDisliked == false)
+        {
+            isLiked = true;
+        }
+        if(isLiked == false && isDisliked == true)
+        {
+            isLiked = true;
+            isDisliked = false;
+        }
+        checkText();
+
+
+    }
+
+    public void ClickDislike()
+    {
+        if (isLiked == false && isDisliked == false)
+        {
+            isDisliked = true;
+        }
+        if (isLiked == true && isDisliked == false)
+        {
+            isLiked = false;
+            isDisliked = true;
+        }
+
+        checkText();
+        
+    }
+
+    void checkText()
+    {
+        if(isLiked)
+        {
+            likeButtonText.text = "";
+            dislikeButtonText.text = "";
+        }
+        else if(isDisliked)
+        {
+            likeButtonText.text = "";
+            dislikeButtonText.text = "";
+        }
+    }
+
+
+}
