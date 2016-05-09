7e788bea4b59b2097f28d12568e289b5ce3a54f8
Lander Verschueren <Lander Verschueren>
2016-05-09 23:03:54 +0200

    projectpagnina

--- web/projectantwerpen/resources/views/projects/individual_project.blade.php
+++ web/projectantwerpen/resources/views/projects/individual_project.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 	<div class="container">
 		<div class="row">
-			<div class="col-md-12 form_general" id="project">
+			<div class="col-md-12 form_general project" id="project">
 				<div class="panel panel-default">
 					<div class="panel-heading">
 						<h4>Titel project</h4>
@@ -14,9 +14,9 @@
 					    		<img src="{{ URL::asset('images/meir-2.png') }}" alt="">
 					    	</section>
 					    	<section>
-					    		<label>Einddatum</label>
-					    		<label>kostprijs</label>
-					    		<label>categorie</label>
+					    		<p>Einddatum</p>
+					    		<p>kostprijs</p>
+					    		<p>categorie</p>
 					    	</section>
 				    	</section>
 				    	<section class="timeline">fase</section>
