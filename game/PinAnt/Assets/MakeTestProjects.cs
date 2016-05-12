bfe93353d3f880b22426382e5f914db4895570f2
Bert Van Hove <bert.vanhove@student.kdg.be>
2016-05-13 00:10:52 +0200

    newScrollBar

---
+++ game/PinAnt/Assets/MakeTestProjects.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MakeTestProjects : MonoBehaviour {
+  public static int projects;
+	// Use this for initialization
+	void Start () {
+    projects = GetCanvasSize.nrOfProjects;
+    Debug.Log("nrprojects = " + projects);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+  public void newProject()
+  {
+    projects++;
+    Debug.Log("nrprojects = " + projects);
+  }
+}
