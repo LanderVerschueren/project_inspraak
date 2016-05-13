88373fabd26390fc6445174ccac81252d6ca6c2d
LanderVerschueren <verschueren@live.nl>
2016-05-13 13:06:59 +0200

    fix

--- web/projectantwerpen/app/Http/Controllers/AdminController.php
+++ web/projectantwerpen/app/Http/Controllers/AdminController.php
@@ -3,21 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Requests;
 use App\Project;
+use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-class ProjectController extends Controller
+class AdminController extends Controller
 {
-	public function __construct() {
+    public function __construct() {
 		//Only when someone needs to be authenticated to acces the page
-		//$this->middleware('auth');
+		$this->middleware('auth');
 	}
 
 	public function index() {
 		
 		$projects = Project::all();
 
-		return view('projects/projects', ['projects' => $projects]);
+		return view('projects/projectslist', ['projects' => $projects]);
 	}
 }
--- web/projectantwerpen/app/Http/routes.php
+++ web/projectantwerpen/app/Http/routes.php
@@ -23,3 +23,4 @@ Route::get('/', 'HomeController@index');
 Route::get('/projecten', 'ProjectController@index');
 
 Route::get('/project/{id}', 'IndividualProjectController@index');
+Route::get('/projectslist', 'AdminController@index');
