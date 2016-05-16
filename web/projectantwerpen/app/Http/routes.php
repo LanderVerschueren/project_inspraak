2b035c0fca6eb39a2ed905f60136ae8e119deb9a
Lander Verschueren <Lander Verschueren>
2016-05-16 14:08:43 +0200

    fix routes

--- web/projectantwerpen/app/Http/routes.php
+++ web/projectantwerpen/app/Http/routes.php
@@ -11,20 +11,20 @@
 |
 */
 
-/*Route::get('/', function () {
-    return view('home');
-});*/
+Route::group([ 'middleware' => 'web' ], function() {
+	Route::auth();
 
-Route::auth();
+	Route::get('/home', 'HomeController@index');
+	Route::get('/', 'HomeController@index');
 
-Route::get('/home', 'HomeController@index');
-Route::get('/', 'HomeController@index');
+	Route::get('/projecten', 'ProjectController@index');
 
-Route::get('/projecten', 'ProjectController@index');
-
-Route::get('/project/{id}', 'IndividualProjectController@index');
-Route::get('/projectslist', 'AdminController@index');
-Route::get('/project/follow/{id}', 'IndividualProjectController@follow');
+	Route::get('/project/{id}', 'IndividualProjectController@index');
+	Route::get('/projectslist', 'AdminController@index');
+	Route::get('/project/follow/{id}', 'IndividualProjectController@follow');
+	Route::get('/project/unfollow/{id}', 'IndividualProjectController@unfollow');
+	Route::get('/search', 'SearchController@index');
+});
 
 Route::group(['prefix' => 'api'], function() {
 	Route::get('api' , 'APIController@request');
