4f90102d2ef8805d677800e7d1032ef5604aacf0
Lander Verschueren <Lander Verschueren>
2016-05-16 14:58:51 +0200

    middleware admin

--- web/projectantwerpen/app/Http/routes.php
+++ web/projectantwerpen/app/Http/routes.php
@@ -11,8 +11,10 @@
 |
 */
 
+Route::auth();
+
 Route::group([ 'middleware' => 'web' ], function() {
-	Route::auth();
+	
 
 	Route::get('/home', 'HomeController@index');
 	Route::get('/', 'HomeController@index');
