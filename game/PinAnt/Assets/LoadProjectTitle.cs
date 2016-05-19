60d7de9207b0267fff653298575f7b7d749d8eb3
Bert Van Hove <bert.vanhove@student.kdg.be>
2016-05-19 18:05:27 +0200

    Zieke commit

---
+++ game/PinAnt/Assets/LoadProjectTitle.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LoadProjectTitle : MonoBehaviour {
+  public GameObject completeProjectPanel;
+
+  private int currentProject;
+
+  public GameObject titlePanel;
+  public GameObject likePanel;
+  public GameObject dislikePanel;
+  public GameObject questionPanel;
+  public GameObject extraInfoPanel;
+
+	// Use this for initialization
+	void Start () {
+
+    currentProject = int.Parse(completeProjectPanel.name.Replace("Project", "")); //haal projectnr uit naam
+    Debug.Log(currentProject);
+
+    titlePanel.GetComponentInChildren<Text>().text = LoadProjectSettings.projectList[currentProject].title;
+    likePanel.GetComponentInChildren<Text>().text = LoadProjectSettings.projectList[currentProject].likes;
+    dislikePanel.GetComponentInChildren<Text>().text = LoadProjectSettings.projectList[currentProject].dislikes;
+    questionPanel.GetComponentInChildren<Text>().text = LoadProjectSettings.projectList[currentProject].question;
+    extraInfoPanel.GetComponentInChildren<Text>().text = LoadProjectSettings.projectList[currentProject].info;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
